2373afb900e66a255ac59b977c675f0298e537ab
Averyyy <58547284+Averyyy@users.noreply.github.com>
2022-04-20 18:03:32 -0400

    fixed card error

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,40 +1,30 @@
 import React from 'react'
-import axios from 'axios'
-import Card from '../components/card'
-// import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Xt } from 'xtendui'
+import 'xtendui/src/toggle'
 
-export default function Blog({environment}) {
-  const [blogs, setBlogs] = React.useState([{'title': 'Loading...', 'content': 'Loading...', 'username': 'Loading...'}]);
-  React.useEffect(() => {
-    axios.get(environment+'/api/blogs')
-      .then(res => {
-        setBlogs(res.data);
-      });
-    }, [])
-
-
+export default function Card({props, env}) {
+    console.log(props)
+  const title = props.title || 'Title';
+  const content = props.content || 'Content';
+  const username = props.username || 'Username';
   return (
-    <>
-    <div className='absolute left-12 top-20 font-bold text-4xl'>Blog Plaza</div>
-    <a
-    // type="button"
-    className="xt-button absolute right-12 top-20 py-2.5 px-3.5 text-sm rounded-md  leading-snug tracking-wider uppercase
-    font-semibold bg-blue-500
-    text-white xt-links-inverse bg-primary-500 border-transparent border transition 
-    hover:text-white hover:bg-primary-600 hover:border-primary-700 hover:-translate-y-1 hover:shadow-lg 
-    active:text-white active:bg-primary-700 active:translate-y-0 active:shadow-md 
-    on:text-white on:bg-primary-700 on:translate-y-0 on:shadow-md" href="/blogcreate">
-    Create Your Blog!
-    </a>
-    <div className="absolute top-36">
-    {blogs.map(blog => (
-    <div className="inline-block justify-center">
-    <Card props={blog} env={environment}/>
-    </div>))}
+    <div>
+      <div className="xt-card block w-60 rounded-2xl text-gray-900 xt-links-default bg-gray-100 scale-75">
+        <div className="xt-media-container bg-gray-200 rounded-t-2xl h-40">
+          <img className="xt-media object-cover" src={env+"/blo.jpeg"} loading="lazy" alt="" />
+        </div>
+        <div className="p-7 sm:p-9 text-base">
+          <div className="xt-h4">{title}</div>
+          <p>
+            {content}
+          </p>
+        </div>
+        <div className="p-6 sm:p-8 text-sm rounded-b-2xl bg-primary-100">
+          <p>
+            <strong>{username}</strong> 
+          </p>
+        </div>
+      </div>
     </div>
-
-
-
-    </>
   )
 }
